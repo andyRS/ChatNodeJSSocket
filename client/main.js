059524527995b1bb3a639b14117b56878190c397
andyRS <andy337@hotmail.es>
2021-01-12 19:09:36 -0400

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://10.0.0.3:6677',{'forceNew':true});
+var socket = io.connect('http://10.0.0.1:6677',{'forceNew':true});
 
 socket.on('messages',function(data){
     console.log(data);
