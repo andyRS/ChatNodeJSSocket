f9c02153f5466976dd791a31390d074761c8cc9b
andyRS <andy337@hotmail.es>
2021-01-12 19:01:54 -0400

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://andyrs.github.io/ChatNodeJSSocket',{'forceNew':true});
+var socket = io.connect('https://https://andyrs.github.io/ChatNodeJSSocket:6677',{'forceNew':true});
 
 socket.on('messages',function(data){
     console.log(data);
